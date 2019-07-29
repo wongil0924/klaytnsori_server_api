7c4617bb49a9548d022173e79fbc23ad1895abad
Taesun94 <tyzlddy@naver.com>
2019-07-30 02:39:33 +0900

    config database

---
+++ database.js
@@ -0,0 +1,7 @@
+module.exports = {
+  host : 'klaytn-database.ciitcrpahoo9.ap-northeast-2.rds.amazonaws.com',
+  user : 'admin',
+  password : '12345678',
+  database : 'klaytndb',
+  port : 3306
+};
